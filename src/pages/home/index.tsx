320da4efba89c4953dae0222d902b91de66f0f52
vinicius martins <vinicius.cross07@gmail.com>
2021-10-28 15:35:17 -0300

    fix:loading

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,31 +6,40 @@ import {styles} from './styles';
 import {Background} from '../../components/Background';
 import {ListDivider} from '../../components/ListDivider';
 import {Heroes} from '../../components/Heroes';
+import {Load} from '../../components/Load';
 
 import {HeroesType, getHeroes} from './getHeroes';
 
 export const Home: React.FC = () => {
   const [heroes, setHeroes] = useState<Array<HeroesType>>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function searchHeroes() {
+      setLoading(true);
       const response = await getHeroes();
       setHeroes(response);
+      setLoading(false);
     }
     searchHeroes();
+    setLoading(false);
   }, []);
 
   return (
     <Background>
-      <FlatList
-        data={heroes}
-        keyExtractor={item => item.id.toString()}
-        renderItem={({item}) => <Heroes data={item} />}
-        ItemSeparatorComponent={() => <ListDivider />}
-        contentContainerStyle={{paddingBottom: 69}}
-        style={styles.matches}
-        showsVerticalScrollIndicator={false}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <FlatList
+          data={heroes}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({item}) => <Heroes data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          contentContainerStyle={{paddingBottom: 69}}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </Background>
   );
 };
